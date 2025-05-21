53d2f811a2e80a9f2f712be49c8cfcefcbef255b
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-05-21 19:15:36 +0300

    Update tests

--- datastore/entry_test.go
+++ datastore/entry_test.go
@@ -6,12 +6,15 @@ import (
 )
 
 func TestEntry_Encode(t *testing.T) {
-	raw := Encode(entry{"key", "value"})
+	raw := Encode(entry{"key", "value", 0})
 	e, _ := ReadEntry(bytes.NewReader(raw), 0)
 	if e.key != "key" {
 		t.Error("incorrect key")
 	}
 	if e.value != "value" {
 		t.Error("incorrect value")
+	}
+	if e.kind != 0 {
+		t.Error("incorrect kind")
 	}
 }
