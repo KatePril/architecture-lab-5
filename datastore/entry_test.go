64f98e3e6d7eec798ccd7eef7c4690e4f73379a8
Andrii-Detix <aivancisin6@gmail.com>
2025-05-21 10:28:26 +0300

    Add tests to delete method

--- datastore/entry_test.go
+++ datastore/entry_test.go
@@ -6,12 +6,15 @@ import (
 )
 
 func TestEntry_Encode(t *testing.T) {
-	raw := Encode(entry{"key", "value"})
+	raw := Encode(entry{"key", "value", 1})
 	e, _ := ReadEntry(bytes.NewReader(raw), 0)
 	if e.key != "key" {
 		t.Error("incorrect key")
 	}
 	if e.value != "value" {
 		t.Error("incorrect value")
+	}
+	if e.isDeleted != 1 {
+		t.Error("incorrect isDeleted flag")
 	}
 }
